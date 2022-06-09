0ecc122c7f9bc3e1164eefb5f22d6316eae9f954
Reisu <reiyanikubaru@gmail.com>
2022-06-09 16:41:31 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -123,7 +123,7 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) | [GitHub](https://github.com/Reisumi) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://bio.link/reisumi)`);
       if (!args) return interaction.send(Embed);
       else {
         let cmd =
